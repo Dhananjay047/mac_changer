b2a93e1177fe6e5c7cf13d1f16986c77fa6352fd
Dhananjay047 <dhananjay.sahu.586@gmail.com>
2022-10-25 11:30:56 -0400

    added regex

--- mac_changer_V2.py
+++ mac_changer_V2.py
@@ -1,6 +1,8 @@
+#regex to be use to parse output :- ([0-9a-zA-Z]+:[0-9a-zA-Z]+:[0-9a-zA-Z]+:[0-9a-zA-Z]+:[0-9a-zA-Z]+:[0-9a-zA-Z]+)
+#2nd regex (\w{2}:\w{2}:\w{2}:\w{2}:\w{2}:\w{2})
 import subprocess
 import optparse
-
+import re
 def takeArgs():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface_name", help="Name of the interface whose mac you want to change")
@@ -22,7 +24,9 @@ def changeMac(interface_name, new_mac):
     subprocess.call(["ifconfig", interface_name, "down"])
     subprocess.call(["ifconfig", interface_name, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface_name, "up"])
-    print("[+] changed mac of " + interface_name)
+    result = subprocess.check_output(["ifconfig", interface_name])
+    reg_result = re.search(r"(\w{2}:\w{2}:\w{2}:\w{2}:\w{2}:\w{2})",result)
+    print("\n[+] changed mac of " + interface_name + " to " + reg_result.group(0))
 
 #body starts here
 
